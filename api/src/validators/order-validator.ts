93a8a61d538fb7853220be6e62f44094c6b4118d
Douglas <douglasd3v.contato@gmail.com>
2026-08-26 12:30:30 -0300

    feat: add order validator

---
+++ api/src/validators/order-validator.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { PaymentMethod } from "../generated/prisma/enums.js";
+
+const orderItemSchema = z.object({
+  productId: z.uuid(),
+  quantity: z
+    .number()
+    .int()
+    .min(1, "The number of items cannot be less than 1."),
+});
+
+const createOrderSchema = z.object({
+  customerName: z
+    .string()
+    .trim()
+    .min(3, "The name field must contain at least 3 characters."),
+  paymentMethod: z.enum(PaymentMethod),
+  items: z
+    .array(orderItemSchema)
+    .min(1, "Order must contain at least one item.")
+    .refine(
+      (items) => {
+        const productIds = items.map((item) => item.productId);
+        return productIds.length === new Set(productIds).size;
+      },
+      {
+        message: "Order cannot contain duplicate products.",
+        path: [],
+      },
+    ),
+});
+
+type CreateOrderInput = z.infer<typeof createOrderSchema>;
+
+export { createOrderSchema, type CreateOrderInput };
